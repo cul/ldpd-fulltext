dd5854008f77f0ec423faabafe43d798d5d0e711
Benjamin Armintor <armintor@gmail.com>
2015-06-09 14:06:08 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ldpd_fulltext
+
+A search of the fulltext search built by the [bancha-solr](https://github.com/cul/bancha-solr "bancha-solr library") library
---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '90009e11597ac3fa9c85c9a5aa76e46b0032cf463c2ce4578183d595a0f925395938cee9c94015cefa6b4b114bbe91fd0603ca4094877244a3f5473d823a60a7'
+
