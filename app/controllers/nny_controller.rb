46bdad8b535035821d0f09f3d5127ee1f7681c51
Benjamin Armintor <armintor@gmail.com>
2018-08-27 17:12:10 -0400

    v0.4.4 fix NNY facet config

--- app/controllers/nny_controller.rb
+++ app/controllers/nny_controller.rb
@@ -5,11 +5,13 @@ class NnyController < FullTextController
   configure_blacklight do |config|
     config.facet_fields['author_ssi'].label = 'Interviewee'
     config.facet_fields.delete('collection_ssim')
-    config.add_facet_field 'interview_num_ssi',
-     sort: 'index', after:'author_ssi', limit: true, label: 'Interview',
-     solr_params: { 'facet.mincount' => 1 }
+    config.facet_fields['interview_num_ssi'].if = true
+    config.facet_fields['interview_num_ssi'].sort = 'index'
+    config.facet_fields['interview_num_ssi'].after = 'author_ssi'
+    config.facet_fields['interview_num_ssi'].limit = true
+    config.facet_fields['interview_num_ssi'].solr_params = { 'facet.mincount' => 1 }
     config.add_facet_field 'session_num_isi',
-     sort: 'index', after:'interview_num_isi', limit: false, label: 'Session',
+     sort: 'index', after:'interview_num_ssi', limit: false, label: 'Session',
      solr_params: { 'facet.mincount' => 1 }
     #config.add_facet_field 'interviewee', pivot: ['author_ssi', 'interview_num_ssi'],
     #  label: 'Interviewee', sort: 'index'
