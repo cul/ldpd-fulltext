aa033d9b06727be8eddd3ed5d3ebe63e08acd048
Benjamin Armintor <armintor@gmail.com>
2025-10-13 16:14:59 -0400

    rails app:update for v7.0.x

--- config/application.rb
+++ config/application.rb
@@ -10,21 +10,12 @@ module TextCollections
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
 
-  # Custom directories with classes and modules you want to be autoloadable.
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
     config.autoload_paths += %W(#{config.root}/app/presenters #{config.root}/app/components)
     config.eager_load_paths += %W(#{config.root}/lib)
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_25_212407) do
+ActiveRecord::Schema[6.1].define(version: 2022_02_25_212407) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
